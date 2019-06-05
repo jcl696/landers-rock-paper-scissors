ffe649e6f0e74d55ef5255144effa1626dc0513b
Joe Landers <jcl696@stern.nyu.edu>
2019-06-05 19:45:58 -0400

    determining the winner
    conditional statements on different choices and which choice wins

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 #game.py
 import random 
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
 
@@ -14,7 +16,7 @@ print("User selection:",user_selection)
 
 # validate inputs
 
-game_options = ["rock", "paper", "scissor"]
+game_options = ["rock", "paper", "scissors"]
 
 
 if user_selection not in game_options:
@@ -34,7 +36,29 @@ computer_selection = random.choice(game_options)
 print("-----------------")
 
 print("Computer selection:", computer_selection)
+
+
 #determine the winner
+
+# rock beats scissors, scissors beats paper, paper beats rock
+# same selections is a tie
+# can either do conditional logic (if statements) or dictionary
+
+if user_selection == computer_selection:
+    print ("TIE")
+elif user_selection == "rock" and computer_selection == "paper":
+    print ("PAPER")
+elif user_selection == "rock" and computer_selection == "scissors":
+    print("ROCK")
+elif user_selection == "paper" and computer_selection == "scissors":
+    print("SCISSORS")
+elif user_selection == "paper" and computer_selection == "rock":
+    print("PAPER")
+elif user_selection == "scissors" and computer_selection == "paper":
+    print("SCISSORS")
+elif user_selection == "scissors" and computer_selection == "rock":
+    print("ROCK")
+
 
 
 #display final outputs / outcomes

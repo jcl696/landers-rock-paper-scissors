01f1a8732b8eac67d36a55593f1333ef9723ba8c
Joe Landers <jcl696@stern.nyu.edu>
2019-06-05 18:55:50 -0400

    user_input
    commit with ability to collect the user input

--- game.py
+++ game.py
@@ -1,3 +1,25 @@
 #game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+#capture input
+
+user_selection = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without quotes):")
+
+print ("-----------------")
+
+print("User selection:",user_selection)
+
+
+# validate inputs
+
+
+
+# generate computer selection
+
+
+#determine the winner
+
+
+#display final outputs / outcomes

a38b3071d5b583c2c78b82f44f6aa6338eb46b6d
Joe Landers <jcl696@stern.nyu.edu>
2019-06-05 20:50:38 -0400

    example test

--- game.py
+++ game.py
@@ -1,134 +1,143 @@
 #game.py
 import random
-#import random 
+
+def my_message():
+    return "HELLO"
+
+# only if this script is executed from the command line should we do all the stuff inside
+if __name__ == "__main__":
 
 
-#print("Rock, Paper, Scissors, Shoot!")
-#
-#
-##capture input
-#
-#user_selection = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without quotes):")
-#
-#print ("-----------------")
-#
-#print("User selection:",user_selection)
-#
-#
-## validate inputs
-#
-#game_options = ["rock", "paper", "scissors"]
-#
-#
-#if user_selection not in game_options:
-#    print("Invalid selection, please run again")
-#    exit()
-#
-#
-#
-## generate computer selection
-#
-#print("Generating...")
-#
-#
-#
-#computer_selection = random.choice(game_options)
-#
-#print("-----------------")
-#
-#print("Computer selection:", computer_selection)
-#
-#
-##determine the winner
-#
-## rock beats scissors, scissors beats paper, paper beats rock
-## same selections is a tie
-## can either do conditional logic (if statements) or dictionary
-#
-###if user_selection == computer_selection:
-   # print ("TIE")
-##elif user_selection == "rock" and computer_selection == "paper":
-##    print ("PAPER")
-##elif user_selection == "rock" and computer_selection == "scissors":
-##    print("ROCK")
-##elif user_selection == "paper" and computer_selection == "scissors":
-##    print("SCISSORS")
-##elif user_selection == "paper" and computer_selection == "rock":
-##    print("PAPER")
-##elif user_selection == "scissors" and computer_selection == "paper":
-##    print("SCISSORS")
-##elif user_selection == "scissors" and computer_selection == "rock":
-##    print("ROCK")
-##
-#
 
-#display final outputs / outcomes
-
-# game.py
+        #import random 
 
 
-print("Rock, Paper, Scissors, Shoot!")  # this is also a comment
+        #print("Rock, Paper, Scissors, Shoot!")
+        #
+        #
+        ##capture input
+        #
+        #user_selection = input("Please choose one of the following options: 'rock', 'paper', or 'scissors' (without quotes):")
+        #
+        #print ("-----------------")
+        #
+        #print("User selection:",user_selection)
+        #
+        #
+        ## validate inputs
+        #
+        #game_options = ["rock", "paper", "scissors"]
+        #
+        #
+        #if user_selection not in game_options:
+        #    print("Invalid selection, please run again")
+        #    exit()
+        #
+        #
+        #
+        ## generate computer selection
+        #
+        #print("Generating...")
+        #
+        #
+        #
+        #computer_selection = random.choice(game_options)
+        #
+        #print("-----------------")
+        #
+        #print("Computer selection:", computer_selection)
+        #
+        #
+        ##determine the winner
+        #
+        ## rock beats scissors, scissors beats paper, paper beats rock
+        ## same selections is a tie
+        ## can either do conditional logic (if statements) or dictionary
+        #
+        ###if user_selection == computer_selection:
+        # print ("TIE")
+        ##elif user_selection == "rock" and computer_selection == "paper":
+        ##    print ("PAPER")
+        ##elif user_selection == "rock" and computer_selection == "scissors":
+        ##    print("ROCK")
+        ##elif user_selection == "paper" and computer_selection == "scissors":
+        ##    print("SCISSORS")
+        ##elif user_selection == "paper" and computer_selection == "rock":
+        ##    print("PAPER")
+        ##elif user_selection == "scissors" and computer_selection == "paper":
+        ##    print("SCISSORS")
+        ##elif user_selection == "scissors" and computer_selection == "rock":
+        ##    print("ROCK")
+        ##
+        #
 
-# CAPTURE INPUTS
+        #display final outputs / outcomes
 
-user_choice = input(
-    "Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
+        # game.py
 
-print("--------------")
-print("USER CHOICE:", user_choice)
 
-# VALIDATE INPUTS
+        print("Rock, Paper, Scissors, Shoot!")  # this is also a comment
 
-options = ["rock", "paper", "scissors"]
+        # CAPTURE INPUTS
 
-if user_choice not in options:
-    print("INVALID SELECTION, PLEASE TRY AGAIN...")
-    exit()
+        user_choice = input(
+            "Please choose one of the following options: 'rock', 'paper', or 'scissors' (without the quotes):")
 
-# GENERATE COMPUTER SELECTION
+        print("--------------")
+        print("USER CHOICE:", user_choice)
 
-computer_choice = random.choice(options)
+        # VALIDATE INPUTS
 
-print("--------------")
-print("GENERATING...")
-print("COMPUTER CHOICE:", computer_choice)
+        options = ["rock", "paper", "scissors"]
 
-# DETERMINE THE WINNER
-#
-# rock beats scissors
-# paper beats rock
-# scissors beats paper
-# same selections is a tie
-#
-# first attribute represents the user, second represents the computer
-winners = {
-    "rock": {
-        "rock": None,
-        "paper": "paper",
-        "scissors": "rock",
-    },
-    "paper": {
-        "rock": "paper",
-        "paper": None,
-        "scissors": "scissors",
-    },
-    "scissors": {
-        "rock": "rock",
-        "paper": "scissors",
-        "scissors": None,
-    },
-}
+        if user_choice not in options:
+            print("INVALID SELECTION, PLEASE TRY AGAIN...")
+            exit()
 
-winning_choice = winners[user_choice][computer_choice]
+        # GENERATE COMPUTER SELECTION
 
-# DISPLAY FINAL OUTPUTS / OUTCOMES
+        computer_choice = random.choice(options)
 
-if winning_choice:
-    if winning_choice == user_choice:
-        print("YOU WON")
-    elif winning_choice == computer_choice:
-        print("YOU LOST")
-else:
-    print("TIE")
+        print("--------------")
+        print("GENERATING...")
+        print("COMPUTER CHOICE:", computer_choice)
 
-print("Thanks for playing. Please play again!")
+        # DETERMINE THE WINNER
+        #
+        # rock beats scissors
+        # paper beats rock
+        # scissors beats paper
+        # same selections is a tie
+        #
+        # first attribute represents the user, second represents the computer
+        winners = {
+            "rock": {
+                "rock": None,
+                "paper": "paper",
+                "scissors": "rock",
+            },
+            "paper": {
+                "rock": "paper",
+                "paper": None,
+                "scissors": "scissors",
+            },
+            "scissors": {
+                "rock": "rock",
+                "paper": "scissors",
+                "scissors": None,
+            },
+        }
+
+        winning_choice = winners[user_choice][computer_choice]
+
+        # DISPLAY FINAL OUTPUTS / OUTCOMES
+
+        if winning_choice:
+            if winning_choice == user_choice:
+                print("YOU WON")
+            elif winning_choice == computer_choice:
+                print("YOU LOST")
+        else:
+            print("TIE")
+
+        print("Thanks for playing. Please play again!")
--- game_test.py
+++ game_test.py
@@ -1,6 +1,28 @@
 
 #need to have test_ at the beginning of defining a function
 
-def test_example():
-    assert 2 ==2  
+from game import my_message
 
+
+#game_test.py
+
+def test_example():
+    assert 2 == 2
+
+def test_my_message():
+    x = my_message()
+    assert x == "HELLO"
+
+#def test_determination_of_the_winner():
+#    assert determine_winner("rock", "rock") == None  # represents a tie
+#    assert determine_winner("rock", "paper") == "paper"
+#    assert determine_winner("rock", "scissors") == "rock"
+#
+#    assert determine_winner("paper", "rock") == "paper"
+#    assert determine_winner("paper", "paper") == None  # represents a tie
+#    assert determine_winner("paper", "scissors") == "scissors"
+#
+#    assert determine_winner("scissors", "rock") == "rock"
+#    assert determine_winner("scissors", "paper") == "scissors"
+#    assert determine_winner("scissors", "scissors") == None  # represents a tie
+#
